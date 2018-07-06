3000110f59a6715da6bb67ce4903a3103ccaa605
Paul Wainer <paul.wainer@gmail.com>
2018-07-05 22:37:33 -0400

    Making progress on get_best_betas_2.py, I changed the syntax of how I create the table. Before I had table_info and InfoLabels as separate lists which I had to maintain separately. Now, I organize them together in an OrderedDict, where the label is the key and the data lists are the values. It is much easier to read, follow, and manage! Still work to do here...

--- scrubbing_processes.py
+++ scrubbing_processes.py
@@ -1,28 +1,163 @@
 from ols_df_transform import create_ols_df
 from utility.decorators import my_time_decorator, empty_decorator
+from utility.finance import daily_returns, get_daily_returns, get_ETF_beta_to_SPY, get_total_return, calculate_percentile_value, get_symbol_from_returns_df, ceiling_scrub, floor_scrub, calculate_average_daily_move_from_returns
+
+from scrub_params import ScrubParams
 
 NO_USE_TIME_DECORATOR = True
 if NO_USE_TIME_DECORATOR == True:
     my_time_decorator = empty_decorator
 
+# The big difference between the two types of scrubbing is that in the original way, I delete the rows from the columns. In the second way, I want to show a NaN value. Which way is better and how big is the difference in methodology?
 @my_time_decorator
-def stock_ceiling_scrub_process(returns_df_to_scrub: 'DataFrame of daily returns for the stock and index',
-                                stock_to_drive_scrubbing,
-                                stock_move_ceiling):
-    """Eliminate data points where the stock moved greater (absolute value) than the specified stock cutoff.
-       This process imposes a celing for the magnitude of stock moves included in the sample."""
-    if stock_move_ceiling < 1.0:
+def stock_ceiling_scrub_process(returns_df_to_scrub: 'DataFrame of daily returns (multiple stocks allowed)',
+                                stock_move_ceiling,
+                                stock_to_drive_scrubbing=False):
+    """Eliminate data points for all stocks on days where the target stock moved more (absolute value) than the specified cutoff.
+        For DataFrames with multiple stocks, the user can specify the target stock.
+        For DataFrames with just one symbol (one column), the target stock is by default the only column."""
+    if stock_move_ceiling:
+    
+        if stock_to_drive_scrubbing == False:
+            stock_to_drive_scrubbing = get_symbol_from_returns_df(returns_df_to_scrub)
+    
         scrubbed_df = returns_df_to_scrub[abs(returns_df_to_scrub[stock_to_drive_scrubbing]) <= stock_move_ceiling]
         return scrubbed_df
+    
     else:
         return returns_df_to_scrub
+
+
+def ceiling_scrub_process_by_percentile(returns_df, percentile_ceiling=100):
+    """Scrub the returns above a certain percentile ceiling. This function is only for one stock at a time."""
+    """Not using at the moment but don't delete yet"""
+    ceiling_value = calculate_percentile_value(returns_df, percentile_ceiling)
+    scrubbed_returns = stock_ceiling_scrub_process(returns_df_to_scrub=returns_df,
+                                                   stock_move_ceiling=ceiling_value)
+    return scrubbed_returns
+
+def largest_abs_value_in_dataframe(dataframe):
+    max_number = dataframe.max().item()
+    min_number = dataframe.min().item()
+    return max(abs(max_number), abs(min_number))
+
+
+class Stock_Ceiling_Params(object):
+    def __init__(self, initial_ceiling, SD_multiplier):
+        self.initial_ceiling = initial_ceiling
+        self.SD_multiplier = SD_multiplier
+
+class Index_Floor_Params(object):
+    def __init__(self, SD_multiplier):
+        self.SD_multiplier = SD_multiplier
+
+
+# Stock Ceiling Params
+STOCK_CEILING_INITIAL_CEILING = .10
+STOCK_CEILING_SD_MULTIPLIER = 3.0
+
+default_stock_ceiling_params  = Stock_Ceiling_Params(initial_ceiling=STOCK_CEILING_INITIAL_CEILING,
+                                                     SD_multiplier=STOCK_CEILING_SD_MULTIPLIER)
+
+# Index Floor Params
+INDEX_FLOOR_SD_MULTIPLIER = 1.5
+
+default_index_floor_params = Index_Floor_Params(SD_multiplier=INDEX_FLOOR_SD_MULTIPLIER)
+
+
+# Best Fit Param
+BEST_FIT_PERCENTILE = 95
+
+stock_ceiling_params = Stock_Ceiling_Params(initial_ceiling=STOCK_CEILING_INITIAL_CEILING,
+                                            SD_multiplier=STOCK_CEILING_SD_MULTIPLIER)
+
+index_floor_params = Index_Floor_Params(SD_multiplier=INDEX_FLOOR_SD_MULTIPLIER)
+
+best_fit_param = BEST_FIT_PERCENTILE
+
+
+
+def get_scrub_params(stock,
+                     index,
+                     lookback=252, 
+                     stock_ceiling_params=default_stock_ceiling_params,
+                     index_floor_params=default_index_floor_params,
+                     best_fit_param=BEST_FIT_PERCENTILE):
+    """Based on the returns data and , determine the scrub_params"""
+    stock_returns_df = get_daily_returns(stock, lookback)
+    index_returns_df = get_daily_returns(index, lookback)
+    
+    stock_ceiling = determine_stock_ceiling_for_scrub_process_from_stock_returns(stock_returns_df, stock_ceiling_params)
+    index_floor = determine_index_floor_for_scrub_process_from_index_returns(index_returns_df, index_floor_params)
+    
+    scrub_params = ScrubParams(stock_ceiling, index_floor, best_fit_param)
+    return scrub_params
+
+def initial_scrub_process(returns_df, STOCK_CEILING_INITIAL_SCRUB):
+    if STOCK_CEILING_INITIAL_SCRUB:
+        initial_scrub = stock_ceiling_scrub_process(returns_df, STOCK_CEILING_INITIAL_SCRUB)
+        return initial_scrub
+    else:
+        return returns_df
+
+def calculate_SD_multiple_from_returns(returns_df, SD_multiplier):
+    average_daily_move = calculate_average_daily_move_from_returns(returns_df)
+    SD_multiple = average_daily_move*SD_multiplier
+    return SD_multiple
+
+
+def determine_stock_ceiling_for_scrub_process_from_stock_returns(returns_df,
+                                                                 stock_ceiling_params=default_stock_ceiling_params):
+    """Dead description: Weinerize the returns with a percentile cutoff. The purpose of the normalization process is to scrub away outsized moves for all stocks, while taking into account that some stocks on average move more than other stocks.
+    A percentile cutoff is intuitive because a 3.0% move for AAPL may be outsized whereas a 3.0% move for SRPT would be standard daily volatility."""
+    initial_scrub = initial_scrub_process(returns_df, stock_ceiling_params.initial_ceiling)
+    stock_ceiling = calculate_SD_multiple_from_returns(initial_scrub, stock_ceiling_params.SD_multiplier)
+    return stock_ceiling
+
+def determine_stock_ceiling_for_scrub_process(stock,
+                                              lookback=252,
+                                              stock_ceiling_params=default_stock_ceiling_params):
+    returns_df = get_daily_returns(stock, lookback)
+    stock_ceiling = determine_stock_ceiling_for_scrub_process_from_stock_returns(returns_df, stock_ceiling_params)
+    return stock_ceiling
+
+
+
+def determine_index_floor_for_scrub_process_from_index_returns(returns_df,
+                                                               index_floor_params=default_index_floor_params):
+    index_floor = calculate_SD_multiple_from_returns(returns_df,
+                                                     SD_multiplier=index_floor_params.SD_multiplier)    
+    return index_floor
+
+def determine_index_floor_for_scrub_process(stock,
+                                            lookback=252,
+                                            stock_ceiling_params=default_index_floor_params):
+    returns_df = get_daily_returns(stock, lookback)
+    stock_ceiling = determine_index_floor_for_scrub_process_from_index_returns(returns_df, stock_ceiling_params)
+    return stock_ceiling
+
+#returns = get_daily_returns('NBIX', lookback=252)
+#stock_ceiling_for_scrub_process = determine_stock_ceiling_for_scrub_process_from_stock_returns(returns)
+
+if __name__ == '__main__':
+    stock_ceiling = determine_stock_ceiling_for_scrub_process('NBIX')
+    print('Stock Ceiling:', stock_ceiling)
+    index_floor = determine_index_floor_for_scrub_process('XBI')
+    print('Index Floor:', index_floor)
+
+    scrub_params = get_scrub_params('NBIX', 'XBI')
+    print(scrub_params)
+
+    #daily_returns = get_daily_returns('XBI', lookback=50)
+    #scrubbed_returns = ceiling_scrub_process_by_percentile(daily_returns, percentile_ceiling=90)
+    #print(scrubbed_returns)
 
 @my_time_decorator
 def index_floor_scrub_process(returns_df_to_scrub: 'DataFrame of daily returns for the stock and index',
                               index_to_drive_scrubbing,
                               index_move_floor):
     """Eliminate data points where the index moved less than the index cutoff"""
-    if index_move_floor > 0:
+    if index_move_floor:
         scrubbed_df = returns_df_to_scrub[abs(returns_df_to_scrub[index_to_drive_scrubbing]) >= index_move_floor]
         return scrubbed_df
     else:
@@ -35,10 +170,10 @@ def best_fit_scrub_process(returns_df_to_scrub: 'DataFrame of daily returns and 
                            percentile_cutoff):
     """Create an OLS regression, and add the best fit line to the returns DataFrame.
        Keep the n percentile data points that have the best fit based on OLS regression"""
-    if percentile_cutoff < 1.0:
+    if percentile_cutoff:
         ols_df_to_scrub = create_ols_df(returns_df_to_scrub)
 
-        best_fit_cutoff = ols_df_to_scrub['error_squared'].quantile(percentile_cutoff)
+        best_fit_cutoff = ols_df_to_scrub['error_squared'].quantile(percentile_cutoff/100)
         scrubbed_ols_df = ols_df_to_scrub[ols_df_to_scrub['error_squared'] <= best_fit_cutoff]
         scrubbed_df = scrubbed_ols_df.loc[:, [stock, index]]
         return scrubbed_df
--- utility/finance.py
+++ utility/finance.py
@@ -1,38 +1,41 @@
-import datetime as dt
 import numpy as np
-import pandas as pd
 import pickle
 import math
-import decimal
-import copy
-import pprint
-import matplotlib.pyplot as plt
-import statsmodels.formula.api as sm
-import pylab
-import scipy.stats as ss
-import statsmodels.api as sm
-from functools import reduce
-from sklearn.linear_model import LinearRegression
-from utility.decorators import my_time_decorator
-from scipy.interpolate import interp1d, UnivariateSpline
-import logging
+from collections import namedtuple
+
 
 import sys
-sys.path.append('/home/paul/Paulthon/data')
+sys.path.append('/home/paul/Paulthon')
 
+# Paul Data
 from data.finance import PriceTable
-# Paul Modules
-from ols import OLS
-from ols2 import OLS as MainOLS
 
-# I moved these over to data.finance but keeping here just in case.
-#Best_Betas = pickle.load(open('Best_Betas.pkl', 'rb'))
-#SPY_Betas_Raw = pickle.load(open('SPY_Betas_Raw.pkl', 'rb'))
-#SPY_Betas_Scrubbed = pickle.load(open('SPY_Betas_Scrubbed.pkl', 'rb'))
+# Brought this over from get_best_betas_2.py since I use the constants in many of the formulas
+SD_Cutoff_Params = namedtuple('SD_Cutoff_Params', ['Stock_SD_Cutoff', 'Index_SD_Cutoff', 'Stock_SD_Percentile_Cutoff', 'Index_SD_Percentile_Cutoff'])
 
-def daily_returns(price_table: 'df of prices') -> 'df of daily_returns':
-    return price_table / price_table.shift(-1) - 1
+STOCK_SD_CUTOFF = 8.0
+INDEX_SD_CUTOFF = 1.75
+STOCK_SD_PERCENTILE_CUTOFF = 90
+INDEX_SD_PERCENTILE_CUTOFF = 100
+LOOKBACK_DEFAULT = 400
+sd_cutoff_params = SD_Cutoff_Params(STOCK_SD_CUTOFF, INDEX_SD_CUTOFF, STOCK_SD_PERCENTILE_CUTOFF, INDEX_SD_PERCENTILE_CUTOFF)
 
+
+# Utility Math Functions
+def calculate_percentile_value(numbers: 'list of numbers', percentile):
+    return np.nanpercentile(abs(numbers), percentile)
+
+def calculate_HV_from_returns(returns):
+    """Return the HV calculation for an array of returns. NaN values in the data are allowed, and will be ignored"""
+    return np.nanstd(returns)*math.sqrt(252)
+
+def calculate_average_daily_move_from_HV(HV):
+    return HV / math.sqrt(252)
+
+def calculate_average_daily_move_from_returns(returns: 'df of returns'):
+    return np.nanstd(returns)
+
+# Get ETF beta to SPY from a pre-saved table
 def get_ETF_beta_to_SPY(ETF):
     try:
         ETF_betas = pickle.load(open('/home/paul/Paulthon/DataFiles/Betas/ETF_betas.pkl', 'rb'))
@@ -42,10 +45,118 @@ def get_ETF_beta_to_SPY(ETF):
         print("{} is not in the ETF beta table".format(ETF))
         return 1.0
 
+
+# Get Symbol
+def get_symbol_from_returns_df(returns_df):
+    return returns_df.columns[0]
+
+
+# Get Stock Prices
 def get_stock_prices_over_lookback(stock, lookback):
+    """Keeping this for now but using the function below in practice"""
     stock_prices = PriceTable.loc[:, stock].head(lookback)
     stock_prices = stock_prices[stock_prices.notnull()]
     return stock_prices
+
+def get_stock_prices(stock, lookback=252):
+    """Return a DataFrame of prices for a given stock and lookback"""
+    price_table = PriceTable.loc[:, [stock]][::-1].head(lookback)
+    return price_table
+
+
+# Get Returns
+def daily_returns(price_table: 'df of prices') -> 'df of daily_returns':
+    return price_table / price_table.shift(-1) - 1
+
+def get_daily_returns(stock, lookback=252):
+    """Return a DataFrame of daily returns for a given stock and lookback"""
+    price_table = get_stock_prices(stock, lookback)
+    return daily_returns(price_table)
+
+
+
+#returns = get_daily_returns('XBI', lookback=20)
+#symbol = get_symbol_from_returns_df(returns)
+#print(symbol)
+
+
+# Scrubbing Functionality
+def scrub_func(daily_return, cutoff, reverse_scrub=False):
+    """Scrub a data point based on the specified cutoff"""
+    if not reverse_scrub:
+        if abs(daily_return) < cutoff:
+            return daily_return
+        else:
+            return np.NaN
+    else:
+        if abs(daily_return) > cutoff:
+            return daily_return
+        else:
+            return np.NaN
+
+def ceiling_scrub(data_point, ceiling):
+    if abs(data_point) < ceiling:
+        return data_point
+    else:
+        return np.NaN
+
+def floor_scrub(data_point, floor):
+    if abs(data_point) > floor:
+        return data_point
+    else:
+        return np.NaN
+
+
+
+# Not using this as of now
+def scrub_returns(returns,
+                  percentile_cutoff=100,
+                  reverse_scrub=False):
+    """Scrub an array of returns based on a percentile_cutoff. The percentile_cutoff eliminates data points with the largest moves above the specified percentile_cutoff"""
+    scrub_cutoff = calculate_percentile(returns, percentile_cutoff)
+    returns = [scrub_func(daily_return, scrub_cutoff, reverse_scrub) for daily_return in returns.iloc[:, 0].tolist()]
+    return returns
+
+
+
+def get_scrubbed_HV_for_stock(stock,
+                              lookback,
+                              percentile_cutoff = STOCK_SD_PERCENTILE_CUTOFF):
+    """Return the scrubbed HV for a stock over a specified lookback. Percentile_cutoff = 100 would return the standard HV calculation"""
+    daily_returns = get_daily_returns(stock, lookback)
+    daily_returns_scrubbed = scrub_returns(daily_returns, percentile_cutoff)
+    return calculate_HV_from_returns(daily_returns_scrubbed)
+
+def get_cutoff(stock,
+              lookback = LOOKBACK_DEFAULT,
+              percentile_cutoff = 100,
+              std_dev_cutoff = 2):
+    HV = get_scrubbed_HV_for_stock(stock, lookback, percentile_cutoff)
+    return HV / math.sqrt(252) * std_dev_cutoff
+
+def get_stock_cutoff(stock,
+                     lookback = LOOKBACK_DEFAULT,
+                     percentile_cutoff = STOCK_SD_PERCENTILE_CUTOFF,
+                     std_dev_cutoff = STOCK_SD_CUTOFF):
+    return get_cutoff(stock, lookback, percentile_cutoff, std_dev_cutoff)
+
+def get_index_cutoff(index,
+                     lookback = LOOKBACK_DEFAULT,
+                     percentile_cutoff = INDEX_SD_PERCENTILE_CUTOFF,
+                     std_dev_cutoff = INDEX_SD_CUTOFF):
+    return get_cutoff(index, lookback, percentile_cutoff, std_dev_cutoff)
+
+
+def get_scrub_params_from_sd_cutoff_params(stock,
+                                        index,
+                                        lookback,
+                                        sd_cutoff_params,
+                                        percentile_cutoff):
+    index_cutoff = get_index_cutoff(index, lookback, sd_cutoff_params.Index_SD_Percentile_Cutoff, sd_cutoff_params.Index_SD_Cutoff)
+    
+    stock_cutoff_raw = get_stock_cutoff(stock, lookback, sd_cutoff_params.Stock_SD_Percentile_Cutoff, sd_cutoff_params.Stock_SD_Cutoff)
+    stock_cutoff = max(.02 + index_cutoff*2, stock_cutoff_raw)
+    return ScrubParams(stock_cutoff, index_cutoff, percentile_cutoff)
 
 def get_total_return(stock, lookback):
     """Get the total return for a stock over a lookback (num. days)"""
